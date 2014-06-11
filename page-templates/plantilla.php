3b96aac3c9a5c71652f606268d989acf91deef12
Daniel Santarriaga <daniel.santarriaga@gmail.com>
2014-06-11 16:32:02 -0600

    3re Commit: Agregamos la carpeta de las plantillas
    Aqui agregamos más comodamente las plantillas de las paginas que usa Wordpress.

---
+++ page-templates/plantilla.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: Plantilla
+ *
+ * A custom page template without sidebar.
+ *
+ * The "Template Name:" bit above allows this to be selectable
+ * from a dropdown menu on the edit page screen.
+ *
+ * @package WordPress
+ * @subpackage GMK-Theme
+ * @since GMK Wordpress Theme 0.42
+ */
+
+get_header(); ?>
+
+	Contenido
+
+<?php get_footer(); ?>
